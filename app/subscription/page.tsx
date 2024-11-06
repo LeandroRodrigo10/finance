0819a6c69e92a7b7000f0cedf1e026d41a3011de
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-11-06 16:23:15 -0300

    O início do Projeto

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const Subscription = () => {
+    return <h1>página de assinatura</h1>;
+}; 
+
+export default Subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const Transactions = () => {
+    return <h1>página de transações</h1>;
+}; 
+
+export default Transactions;
