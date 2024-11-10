ab2ac927b7d6f2174e67b6b472030bac854ce720
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-11-10 18:25:15 -0300

    problema com data-table

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,20 @@
-const TransactionsPage = () => {
-    return <h1>página de transações</h1>;
+import { ArrowDown, ArrowDownUpIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import {db} from "../_lib/prisma"
+
+const TransactionsPage = async () => {
+    const transaction = await db.transaction.findMany({})
+    return (
+        <div>
+            <div className="flex w-full items-center justify-between p-6">
+                <h1 className="text-2xl font-bold">Transações</h1>
+                <Button className="rounded-full">
+                    Adicionar Transação
+                    <ArrowDownUpIcon />
+                </Button>
+            </div>
+        </div>
+    );
 }; 
 
 export default TransactionsPage;
