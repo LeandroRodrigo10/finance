c6ba68754db9dde00ae8624ac2114d718d4953eb
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-11-10 20:32:00 -0300

    deu certo

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { Badge } from "@/app/_components/ui/badge";
 import { Transaction, TransactionType } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { Badge, CircleIcon } from "lucide-react"
+import { CircleIcon } from "lucide-react";
+
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
     {
@@ -14,12 +16,30 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
         header: "Tipo",
         cell: ({ row: { original: transaction } }) => {
             if (transaction.type === TransactionType.DEPOSIT) {
-                return <Badge>Depósito</Badge>;
+                return (
+                    <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+                        <CircleIcon className="mr-2 fill-primary" size={10} />
+                        Depósito
+                    </Badge>
+                );
             }
             if (transaction.type === TransactionType.EXPENSE) {
-                return "Despesa";
+                return (
+                    <Badge className="hover:bg-muted font-bold text-danger bg-danger bg-opacity-10">
+                        <CircleIcon
+                            className="fill-danger mr-2" size={10}/>
+                        Despesa
+                    </Badge>
+                );
+
             }
-            return "Investimento";
+            return (
+                <Badge className="hover:bg-muted font-bold text-white bg-white bg-opacity-10">
+                    <CircleIcon
+                        className="fill-white mr-2" size={10} />
+                    Investimento
+                </Badge>
+            );
         }
 
     },
