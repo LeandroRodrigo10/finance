ac22710f8d2aba5ffc1b28e14555d9df6d6f1915
Leandro Rodrigo <117653010+LeandroRodrigo10@users.noreply.github.com>
2024-11-18 21:01:24 -0300

    feat: add last transactions components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,13 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
-import SummaryCards from "./_components/sumary-cards";
-import TimeSelct from "./_components/time-select";
+import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
 import TransactionsPieChart from "./_components/transactions-pie-chart";
 import { getDashboard } from "../_data/get-dashboard";
 import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
+import SummaryCards from "./_components/sumary-cards";
 
 interface HomeProps {
   searchParams: {
@@ -14,38 +15,38 @@ interface HomeProps {
   };
 }
 
-const Home = async ({searchParams: {month}}: HomeProps) => {
+const Home = async ({ searchParams: { month } }: HomeProps) => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-
   const monthIsInvalid = !month || !isMatch(month, "MM");
   if (monthIsInvalid) {
-    redirect("?month=01");
+    redirect(`?month=${new Date().getMonth() + 1}`);
   }
-
-  const dashboard = await getDashboard(month)
+  const dashboard = await getDashboard(month);
   return (
     <>
       <Navbar />
-      <div className="p-6 space-y-6">
+      <div className="flex h-full flex-col space-y-6 overflow-hidden p-6">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-bold"> Dashboard </h1>
-          <TimeSelct />
+          <h1 className="text-2xl font-bold">Dashboard</h1>
+          <TimeSelect />
         </div>
-        <div className="grid grid-cols-[2fr,1fr]">
-          <div className="flex flex-col gap-6">
-            <SummaryCards investmentsTotal={0} month={month} {...dashboard} />
-            <div className="grid grid-cols-3 grid-row-1 gap-6">
-              <TransactionsPieChart investmentsTotal={0} {...dashboard} />
-              <ExpensesPerCategory expensesPerCategory={dashboard.totalExpensePerCategory}/>
+        <div className="grid h-full grid-cols-[2fr,1fr] gap-6 overflow-hidden">
+          <div className="flex flex-col gap-6 overflow-hidden">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
+              <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
-
   );
 };
 
